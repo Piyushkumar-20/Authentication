4833d4c707c4dad1dde0c461ce7b2733f230cf2d
Piyushkumar-20 <pk6686742@gmail.com>
2026-04-01 20:00:50 +0530

    Drizzle Setup

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,1 +1,15 @@
-// Tables wii be defined here but dont know how 
+import {pgTable, text, uuid, varchar, boolean, timestamp} from 'drizzle-orm/pg-core'
+
+export const userTable = pgTable('user', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    firstName: varchar('first_name', {length: 45}).notNull(),
+    lastName: varchar('last_name', {length: 45}),
+    email: varchar('email', {length: 322}).notNull().unique(),
+    emailVerified: boolean('email_verified').default(false).notNull(),
+    password: varchar('password', {length: 66}).notNull(),
+    salt: text('salt'),
+
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
+    
+})
